f28eb1de92f85cba2cc7a764defd61eaaff1df9b
Nathan Rodrigues Costa <nathan.costa@grupopll.com.br>
2021-12-20 13:43:18 -0300

    código da exclusão

---
+++ config/route.php
@@ -0,0 +1,15 @@
+<?php
+
+use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\ListarCursos;
+use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\Exclusao;
+
+$rotas = [
+    '/listar-cursos' => ListarCursos:: class,
+    '/novo-curso' => FormularioInsercao:: class,
+    '/salvar-curso' => Persistencia:: class,
+    '/excluir-curso' => Exclusao:: class
+];
+
+return $rotas;

4fee16f9e223058642cecd8bad91733b029aa0d4
Senhorracional <88856180+Senhorracional@users.noreply.github.com>
2021-12-20 14:07:28 -0300

    Update listar-cursos.php

--- view/cursos/listar-cursos.php
+++ view/cursos/listar-cursos.php
@@ -1,4 +1,4 @@
-<?php require __DIR__ . '/../inicio-html.php'; ?>
+<?php include __DIR__ . '/../inicio-html.php'; ?>
 
     <a href="/novo-curso" class="btn btn-primary mb-2">
         Novo curso
@@ -6,10 +6,13 @@
 
     <ul class="list-group">
         <?php foreach ($cursos as $curso): ?>
-            <li class="list-group-item">
+            <li class="list-group-item d-flex justify-content-between">
                 <?= $curso->getDescricao(); ?>
+                <a href="/excluir-curso?id=<?= $curso->getId(); ?>" class="btn btn-danger btn-sm">
+    Excluir
+</a>
             </li>
         <?php endforeach; ?>
     </ul>
 
-<?php require __DIR__ . '/../fim-html.php'; ?>
+<?php include __DIR__ . '/../fim-html.php'; ?>
